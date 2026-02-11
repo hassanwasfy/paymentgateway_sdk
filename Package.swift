02edff462ea7fb02b623bd01e29d9d1d9c27b89f
Hassan Wasfy <64452659+hassanwasfy@users.noreply.github.com>
2026-02-11 17:11:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaymentGateway",
-            url: "https://github.com/hassanwasfy/paymentGateway_sdk/releases/download/0.0.1/PaymentGateway.xcframework.zip",
+            url: "https://github.com/hassanwasfy/paymentgateway_sdk/releases/download/0.0.1/PaymentGateway.xcframework.zip",
             checksum: "5028e6fd42e6b0d2d3a6f75a3a0c7b39ff55a581b65cacb4c5856323d9a53ace"
         ),
     ]
