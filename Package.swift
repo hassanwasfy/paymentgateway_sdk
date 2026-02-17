039971b0345f0bc3c41912cb7c43374d2fa9b1c0
Hassan Wasfy <hassanwasfy7@gmail.com>
2026-02-17 14:41:49 +0200

    SPM: update iOS binary 0.0.9

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaymentGateway",
-            url: "https://github.com/hassanwasfy/paymentgateway_sdk/releases/download/0.0.8/PaymentGateway.xcframework.zip",
+            url: "https://github.com/hassanwasfy/paymentgateway_sdk/releases/download/0.0.9/PaymentGateway.xcframework.zip",
             checksum: "6c8ff557fc303c82d65dc7d6677c24332bba6aab88222b8c5575e03b35e4389a"
         ),
     ]
